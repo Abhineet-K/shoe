072833cc2b1955bbc3fba356dcb115efa4147bf3
Abhineet <138810742+Abhineet-K@users.noreply.github.com>
2023-10-06 01:38:45 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import Search from './components/search/search';
 import CurrentWeather from './components/search/current-weather/current-weather';
-import { weather_api_url, weather_api_key } from './api';
+import { weather_api_url} from './api';
 import { useState } from 'react';
 import Forecast from './components/search/forecast/forecast';
+
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setforecast] = useState(null);
+  const api_key = process.env.REACT_APP_API_KEY;
+  
 
   const handleOnSearchChange = (searchData) => {
     const [lat, lon] = searchData.value.split(" ");
 
-    const CurrentWeatherFetch = fetch(`${weather_api_url}/weather?lat=${lat}&lon=${lon}&appid=${weather_api_key}&units=metric`)
-    const forecastFetch = fetch(`${weather_api_url}/forecast?lat=${lat}&lon=${lon}&appid=${weather_api_key}&units=metric`)
+    const CurrentWeatherFetch = fetch(`${weather_api_url}/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
+    const forecastFetch = fetch(`${weather_api_url}/forecast?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
 
     Promise.all([CurrentWeatherFetch, forecastFetch])
       .then(async (respone) => {
